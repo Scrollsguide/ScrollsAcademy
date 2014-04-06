573f136484d409022270d2c161f6213f25181d85
Koen Basten <kbasten@gmail.com>
2014-04-06 21:05:59 +0200

    remove debug things

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,4 @@
 <?php
-	$start2 = microtime(true);
-	
 	$root = dirname(__DIR__);
 	
 	require_once $root . "/classes/Autoloader.php";
@@ -14,5 +12,3 @@
 	$a->init();
 	$a->run();
 	$a->close();
-
-echo ((microtime(true) - $start2) * 1000) . " milliseconds";
